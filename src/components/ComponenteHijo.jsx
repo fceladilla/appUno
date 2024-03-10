60c6836368eee2cf0763de6a83bd65d2c4146a70
Fran <fceladilla@gmail.com>
2024-03-10 18:32:13 +0100

    agregamos estilo al componente hijo y style in line

---
+++ src/components/ComponenteHijo.jsx
@@ -0,0 +1,10 @@
+import style from "../assets/style/ComponenteHijo.module.css";
+function ComponenteHijo() {
+  return (
+    <>
+      <h3 className={style.fondo}>Soy el Componente Hijo </h3>
+    </>
+  );
+}
+
+export default ComponenteHijo;
--- src/components/ComponenteNuevo.jsx
+++ src/components/ComponenteNuevo.jsx
@@ -1,8 +1,12 @@
 import style from "../assets/style/ComponenteNuevo.module.css";
+import ComponenteHijo from "./ComponenteHijo";
 function ComponenteNuevo() {
   return (
     <>
-      <h1 className={style.fondo}>Holis</h1>
+      <h1 style={{ borderBottom: "solid 5px black" }} className={style.fondo}>
+        Holis
+      </h1>
+      <ComponenteHijo></ComponenteHijo>
     </>
   );
 }
